57a8787d37adac5d761e9803eb8164c1f9bca678
Nazar Kapustynskyi <nazarkapustynskiy1996@gmail.com>
2026-01-17 17:33:07 +0100

    Initial commit

---
+++ src/main/java/com/github/nazarkapusta228/toolbox_health_check/ToolboxHealthCheckApplication.java
@@ -0,0 +1,13 @@
+package com.github.nazarkapusta228.toolbox_health_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolboxHealthCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolboxHealthCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nazarkapusta228/toolbox_health_check/ToolboxHealthCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nazarkapusta228.toolbox_health_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolboxHealthCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
